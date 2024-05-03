25e026ed45762f0673bf7cd747706744e54e97e1
Dell <aran.huarte@estudiantat.upc.edu>
2024-05-04 01:45:43 +0200

    El backend chufla mi rey

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from services.user import create_user_service, get_users_service
+from services.user import create_user_service, get_users_service, get_user_service, update_user_service, delete_user_service
 
 user = Blueprint('user', __name__)
 
@@ -8,6 +8,18 @@ user = Blueprint('user', __name__)
 def get_users():
   return get_users_service()
 
+@user.route('/<id>', methods=['GET'])
+def get_user(id):
+  return get_user_service(id)
+
 @user.route('/create', methods=['POST'])
 def create_user():
   return create_user_service()
+
+@user.route('/<id>', methods=['PUT'])
+def update_user(id):
+  return update_user_service(id)
+
+@user.route('/<id>', methods=['DELETE'])
+def delete_user(id):
+  return delete_user_service(id)
--- src/services/user.py
+++ src/services/user.py
@@ -1,5 +1,5 @@
 from flask import request, Response
-from bson import json_util, objectid
+from bson import json_util, ObjectId
 
 from config.mongodb import mongo
 
@@ -8,11 +8,12 @@ from models.User.model import User
 def create_user_service():
     try:
         data = request.get_json()
-        user_name = data.get('userName', None)
+        print(data)
+        user_name = data.get('user_name', None)
         mail = data.get('mail', None)
         password = data.get('password', None)
         
-        if user_name and mail and password:
+        if user_name:
             print(user_name, mail, password)
             new_user = User(user_name=user_name, mail=mail, password=password)
             print(new_user)
@@ -27,9 +28,27 @@ def get_users_service():
     users = mongo.db.users.find()
     result = json_util.dumps(users)
     return Response(result, mimetype='application/json')
-    
-def get_users_service():
-    data = mongo.db.User.find()
+
+def get_user_service(id):
+    data = mongo.db.users.find_one({'_id': ObjectId(id)})
     result = json_util.dumps(data)
     return Response(result, mimetype='application/json')
 
+def update_user_service(id):
+    data = request.get_json()
+    if len(data) == 0:
+      return 'Invalid payload', 400
+    
+    response = mongo.db.users.update_one({'_id': ObjectId(id)}, {'$set': data})
+
+    if response.modified_count >= 1:
+        return 'user updated successfully', 200
+    else:
+        return 'user not found', 404
+
+def delete_user_service(id):
+    response = mongo.db.users.delete_one({'_id': ObjectId(id)})
+    if response.deleted_count >= 1:
+        return 'user deleted successfully', 200
+    else:
+        return 'user not found', 404
