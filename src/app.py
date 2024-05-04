31b8a95e9e2f2c3dd9c5016fbad322a1bd42090e
cristianliebana <cristian.liebana@estudiantat.upc.edu>
2024-05-05 00:02:28 +0200

    cors añadido

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
@@ -10,15 +11,18 @@ config = load_dotenv()
 
 app = Flask(__name__)
 
+# Configura CORS para permitir todas las solicitudes desde cualquier origen
+CORS(app)
+
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 mongo.init_app(app)
 
 @app.route('/')
 def index():
-  return render_template('login.html')
+    return render_template('login.html')
 
 app.register_blueprint(user, url_prefix='/user')
 app.register_blueprint(data, url_prefix='/data')
 
 if __name__ == '__main__':
-  app.run(debug=True, host='10.192.230.83') # Reemplaza '192.168.1.100' con la dirección IP de tu ordenador
+    app.run(debug=True)
